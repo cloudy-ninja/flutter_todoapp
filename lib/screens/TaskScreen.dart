2c43b16795eda3016ac157092b51d3bcf476d6ad
cloudy-ninja <msr0327@outlook.com>
2020-10-05 15:30:09 +0800

    changed UI a little

--- lib/screens/TaskScreen.dart
+++ lib/screens/TaskScreen.dart
@@ -5,41 +5,46 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60, right: 30, bottom: 30, left: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30.00,
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Todo app',
-              style: TextStyle(fontSize: 60, color: Colors.white),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(fontSize: 30, color: Colors.white),
-            ),
-            SizedBox(height: 30),
-            Expanded(
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, right: 30, bottom: 30, left: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.00,
                 ),
+                SizedBox(height: 10),
+                Text(
+                  'Todo app',
+                  style: TextStyle(fontSize: 60, color: Colors.white),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                SizedBox(height: 30),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
