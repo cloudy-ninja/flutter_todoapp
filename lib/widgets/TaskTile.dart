1bd958f977073c5de50c96470a5445592f7a39d1
cloudy-ninja <msr0327@outlook.com>
2020-10-05 16:34:16 +0800

    toggle state of checkbox

--- lib/widgets/TaskTile.dart
+++ lib/widgets/TaskTile.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkBoxCallBack(bool checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(value: false, onChanged: null),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskTileCheckBox(isChecked, checkBoxCallBack),
+    );
+  }
+}
+
+class TaskTileCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Function toggleCheckBoxState;
+  TaskTileCheckBox(this.isChecked, this.toggleCheckBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: toggleCheckBoxState,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
